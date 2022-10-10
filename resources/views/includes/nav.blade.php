64c2a6b1775d706e8433c444f01119a0242bed39
toonvanderlely <75312183+toonvanderlely@users.noreply.github.com>
2022-10-10 11:51:12 +0200

    Added base layout

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,29 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+  
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Navbar brand -->
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">LOGO</a>
+      </div>
+      <!-- Collapsible wrapper -->
+
+      <!-- Left links -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">login</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('includes.head')
+
+    @yield('head')
+</head>
+<body>
+
+@include('includes.nav')
+
+@include('includes.footer')
+</body>

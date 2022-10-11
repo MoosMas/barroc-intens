e79702645438541f835a9727d5e4527310dc44ed
Sam Brands <d286050@edu.rocwb.nl>
2022-10-11 11:52:53 +0200

    Created ContactsController
    Also added resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ContactsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,8 @@ Route::get('/dashboard', function () {
     return view('pages/dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('contacts', ContactsController::class);
+});
+
+require __DIR__ . '/auth.php';

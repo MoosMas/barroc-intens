33c5f116ec13c2382d7aaed9d1f57928fdcf36c7
toonvanderlely <75312183+toonvanderlely@users.noreply.github.com>
2022-10-11 10:29:52 +0200

    Fixed base template

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,7 +7,17 @@
 </head>
 <body>
 
-@include('includes.nav')
+    @include('includes.nav')
 
-@include('includes.footer')
+        <div class="container">
+                <div class="col-md-8">
+                    <div class="center">
+                        @yield('content')
+                    </div>
+                </div>
+        </div>
+        <div class="row">
+            @include('includes.footer')
+        </div>
 </body>
+</html>

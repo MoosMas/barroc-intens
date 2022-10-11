296919e92ddb776d4e08339ba9cb1043cc3b68f9
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2022-10-11 15:56:41 +0200

    changed contact page + added images
    removed firstname and lastname and switched companyname

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,7 +1,7 @@
  @extends('layouts.base')
 
     @section('content')
-        <div class="mx-auto" style="width: 200px;">
+        <div class="mx-auto mt-3" style="width: 200px;">
             <div class="card" style="width: 19rem;">
                 <div class="card-body">
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque eos facilis fugiat iusto minima nisi nobis officia perferendis quam sunt! Cumque dolores ea eos fugit quis. Est nisi rerum temporibus.</p>

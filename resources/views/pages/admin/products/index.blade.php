f558adcb6762f6f8818ac7c110cef60de18af8ed
jenniferwillems <70897990+jenniferwillems@users.noreply.github.com>
2022-10-12 13:10:44 +0200

    products are now displayed on the products page

--- resources/views/pages/admin/products/index.blade.php
+++ resources/views/pages/admin/products/index.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
 
+    @foreach( $products as $product )
+        <div class="product">
+            <img src="img/logo.png" alt="logo">
+            <h5>{{ $product->name }} </h5>
+            <p>{{ $product->description }}</p>
+
+            <button type="button" class="btn btn-dark">Dark</button>
+        </div>
+    @endforeach
 @endsection

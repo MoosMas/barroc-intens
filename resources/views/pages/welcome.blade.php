d590a6cd176556e7f9d45e1c1efac4b72254b0eb
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2022-10-13 14:56:20 +0200

    added images to homepage

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -10,5 +10,23 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card" style="width: 19rem;">
+                    <img src="{{ asset('img/machine-bit-deluxe.png') }}" alt="koffiemachine-deluxe">
+                    <div class="card-body">
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque eos facilis fugiat iusto minima nisi nobis officia perferendis quam sunt! Cumque dolores ea eos fugit quis. Est nisi rerum temporibus.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card" style="width: 19rem;">
+                    <img src="{{ asset('img/machine-bit-light.png') }}" alt="koffiemachine-light">
+                    <div class="card-body">
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque eos facilis fugiat iusto minima nisi nobis officia perferendis quam sunt! Cumque dolores ea eos fugit quis. Est nisi rerum temporibus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     @endsection
 

df8bf19483ea01663e99486d319c95eccb990b6c
Sam Brands <d286050@edu.rocwb.nl>
2022-10-20 11:41:47 +0200

    Closed form tag
    Fixes issue where you were logged out when submitting a form

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -42,6 +42,7 @@
 						                 onclick="event.preventDefault(); this.closest('form').submit();">
 							{{ __('Log Out') }}
 						</x-dropdown-link>
+					</form>
 				@endauth
 			</li>
 		</ul>

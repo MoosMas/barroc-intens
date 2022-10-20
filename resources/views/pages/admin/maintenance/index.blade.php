b407d2d95068dcb4075d6a59d1a2528174323e0d
Sam Brands <d286050@edu.rocwb.nl>
2022-10-20 15:14:41 +0200

    Updated active nav tab

--- resources/views/pages/admin/maintenance/index.blade.php
+++ resources/views/pages/admin/maintenance/index.blade.php
@@ -7,12 +7,12 @@
 		<div class="w-50 d-flex justify-content-end align-items-center">
 			<ul class="nav nav-pills" id="pills-tab" role="tablist">
 				<li class="nav-item" role="presentation">
-					<button class="nav-link" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
+					<button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
 					        type="button" role="tab" aria-controls="pills-home" aria-selected="true"><i
 								class="bi bi-list"></i></button>
 				</li>
 				<li class="nav-item" role="presentation">
-					<button class="nav-link active" id="pills-profile-tab" data-bs-toggle="pill"
+					<button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
 					        data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
 					        aria-selected="false"><i class="bi bi-calendar3"></i></button>
 				</li>
@@ -23,7 +23,7 @@
 	</div>
 
 	<div class="tab-content" id="pills-tabContent">
-		<div class="tab-pane fade" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+		<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
 			<table id="data-table" class="table table-striped table-hover">
 				<thead>
 					<tr>
@@ -54,7 +54,7 @@
 			</table>
 		</div>
 
-		<div class="tab-pane fade show active" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"
+		<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"
 		     tabindex="0">
 			<div id="calendar">
 

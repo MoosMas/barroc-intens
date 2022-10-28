0447cf4f78667db053453d283705ff4e06a8d600
Sam Brands <d286050@edu.rocwb.nl>
2022-10-28 20:59:40 +0200

    Added multiple products selection
    Multiple products can now be added to an invoice (in the frontend. I'm used Alpine JS to create a template for the products dropdown and counter for product amount. When the user wants to add another product, they click the button and the template is cloned and added to the table.

--- resources/views/pages/admin/finance/invoice/create.blade.php
+++ resources/views/pages/admin/finance/invoice/create.blade.php
@@ -13,15 +13,49 @@
             <input type="date" name="date" class="form-control" id="date">
         </div>
 
-        <div class="form-group mb-3">
-            <label for="product" class="form-label">Product</label>
-            <select class="form-select" id="product" name="product_id">
-                <option selected disabled>Selecteer een product</option>
-                @foreach($products as $product)
-                    <option value="{{$product->id}}">{{$product->name}}</option>
-                @endforeach
+        <div class="form-group mb-3" x-data="{}">
+            
+            <table class="table" id="products-table" >
+                <thead>
+                    <tr>
+                        <td class="form-label ps-0">Producten</td>
+                        <th class="text-center">Aantal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <template id="products-template" x-data="{count}">
 
-            </select>
+                        <tr>
+                            <td class="products-fields-container col-8" :id="'products-fields-template-'+count">
+                                <select class="form-select select-product" :name="'products['+ count +']'">
+                                    <option selected disabled>Selecteer een product</option>
+
+                                    @foreach($products as $product)
+                                        <option value="{{$product->id}}">{{$product->name}}</option>
+                                    @endforeach
+
+                                </select>
+                            </td>
+                            
+                            <td x-data="{amount: 0}">
+                                <div class="input-group w-75 mx-auto">
+                                    <button class="btn btn-outline-secondary" type="button"
+                                            @click="if(amount > 0) amount--">-
+                                    </button>
+                                    <input type="text" :name="'products['+ count +'][amount]'" x-model="amount"
+                                           class="form-control text-center">
+                                    <button class="btn btn-outline-secondary" type="button" @click="amount++">+
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    </template>
+                </tbody>
+                
+            </table>
+
+            <button @click="addProduct()" type="button" class="btn btn-outline-primary">Product toevoegen</button>
+
         </div>
 
         <div class="form-group mb-3">
@@ -38,4 +72,20 @@
         <input type="submit" value="Save Item" class="btn btn-primary">
 
     </form>
+
+    <script>
+        let container = document.getElementById('products-table');
+        let template = document.getElementById('products-template');
+        let count = -1;
+        
+        function addProduct(){
+            let clone = template.content.cloneNode(true)
+            container.appendChild(clone);
+            count++;
+        }
+
+		$( document ).ready(() => {
+			addProduct();
+		});
+    </script>
 @endsection

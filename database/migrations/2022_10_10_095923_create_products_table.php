6a4d2d138b2763f4e62a83987cfca0e3a062762d
Sam Brands <d286050@edu.rocwb.nl>
2022-10-31 10:52:03 +0100

    Removed image_path column for products

--- database/migrations/2022_10_10_095923_create_products_table.php
+++ database/migrations/2022_10_10_095923_create_products_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->string('image_path');
             $table->decimal('price');
             $table->foreignId('product_category_id')
                 ->references('id')

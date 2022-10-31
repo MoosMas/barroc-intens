ab39591f81c4d4f2b45b93c0dc1b4ae610188453
Sam Brands <d286050@edu.rocwb.nl>
2022-10-31 16:34:52 +0100

    Renamed user role column to role_id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->foreignId('role')
+            $table->foreignId('role_id')
                 ->references('id')
                 ->on('roles');
             $table->string('password');

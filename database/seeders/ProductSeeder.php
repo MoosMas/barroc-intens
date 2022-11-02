32e74826fac0f5b6b94b49ef4c04aa5f96088e06
toonvanderlely <75312183+toonvanderlely@users.noreply.github.com>
2022-11-02 14:17:11 +0100

    added stuff for the product pages

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -18,25 +18,25 @@ class ProductSeeder extends Seeder
         $products = [
             [
                 'name' => 'Koffiebonen (klein)',
-                'description' => 'Klein zakje koffiebonen',
+                'description' => 'Onze nieuwe koffiebonen smaken dubbel zo goed! Ontdek het dubbel gecertificeerde Douwe Egberts Biologisch & Fairtrade assortiment. Dubbel gecertificeerd, dat smaakt zo dubbel zo goed! Douwe Egberts Dark Roast Espresso Biologisch & Fairtrade is een melange samengesteld uit 100% Fairtrade gecertificeerde Arabica en Robusta bonen. De bonen zijn donker gebrand en krachtige smaak. Het smaakprofiel wordt gekenmerkt door tonen van toffee en chocolade en hinten van dadels en rijpe pruim. De melange is uitstekend geschikt voor een smaakvolle espresso en lungo, maar vormt ook een solide basis voor een robuuste cappuccino of koffie verkeerd.',
                 'price' => 10,
                 'product_category_id' => 2
             ],
             [
                 'name' => 'Koffiebonen (groot)',
-                'description' => 'Groot zakje koffiebonen',
+                'description' => 'Onze nieuwe koffiebonen smaken dubbel zo goed! Ontdek het dubbel gecertificeerde Douwe Egberts Biologisch & Fairtrade assortiment. Dubbel gecertificeerd, dat smaakt zo dubbel zo goed! Douwe Egberts Dark Roast Espresso Biologisch & Fairtrade is een melange samengesteld uit 100% Fairtrade gecertificeerde Arabica en Robusta bonen. De bonen zijn donker gebrand en krachtige smaak. Het smaakprofiel wordt gekenmerkt door tonen van toffee en chocolade en hinten van dadels en rijpe pruim. De melange is uitstekend geschikt voor een smaakvolle espresso en lungo, maar vormt ook een solide basis voor een robuuste cappuccino of koffie verkeerd.',
                 'price' => 15,
                 'product_category_id' => 2
             ],
             [
-                'name' => 'Espresso',
-                'description' => 'Espresso koffiezetapparaat',
+                'name' => 'Espresso koffiezet apparaat',
+                'description' => 'Ben je op zoek naar een perfecte koffie-ervaring? Dat kan heel makkelijk. Met de Barroc intenst koffie machine. Een uniek systeem, dat als enige een hoge capaciteit combineert met een hoge kwaliteit koffie (tot wel 700 kopjes per uur!). Met het gesloten zetsysteem geniet iedereen op elk moment van een perfecte koffie.',
                 'price' => 300,
                 'product_category_id' => 1
             ],
             [
-                'name' => 'Nespresso',
-                'description' => 'Nespresso koffiezetapparaat',
+                'name' => 'Nespresso koffiezet apparaat',
+                'description' => 'Ben je op zoek naar een perfecte koffie-ervaring? Dat kan heel makkelijk. Met de Barroc intenst koffie machine. Een uniek systeem, dat als enige een hoge capaciteit combineert met een hoge kwaliteit koffie (tot wel 700 kopjes per uur!). Met het gesloten zetsysteem geniet iedereen op elk moment van een perfecte koffie.',
                 'price' => 300,
                 'product_category_id' => 1
             ],

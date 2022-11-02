db963df1e8959092511bac4a44865d3f83c356e1
toonvanderlely <75312183+toonvanderlely@users.noreply.github.com>
2022-11-02 14:18:30 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use \App\Http\Controllers\CompanyController;
 use \App\Http\Controllers\MaintenanceController;
 use App\Http\Controllers\UserController;
 use \App\Http\Controllers\OfferController;
+use App\Http\Controllers\RoleController;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,9 +27,23 @@ Route::get('/', function () {
     return view('pages/welcome');
 });
 
+Route::get('/guest/products', function(){
+    $products = Product::all();
+        return view('pages/guest/products/index' ,[
+            'products' => $products
+        ]);
+});
+
+Route::get('/guest/products/{id}', function($id){
+    $product = Product::findOrFail($id);
+        return view('pages/guest/products/show',[
+            'product' => $product
+        ]);
+})->name('product');
+
 Route::get('/pages/guest/contact', function () {
     return view('pages/guest/contact');
-});
+})->name('contact');
 
 Route::get('/maintenance/create', [MaintenanceController::class, 'create']);
 
@@ -45,5 +61,6 @@ Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
     Route::resource('users', UserController::class);
     Route::resource('offers', OfferController::class);
 });
+
 
 require __DIR__ . '/auth.php';

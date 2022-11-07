6f1e16fffc04ad29b336742076017543fd757645
Sam Brands <d286050@edu.rocwb.nl>
2022-11-07 13:42:44 +0100

    Added method to get products in a contract

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Contract extends Model
 {
     use HasFactory;
+    
+    protected $guarded = [];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)->withPivot('amount', 'price_per_product');
+    }
 }

cd0976ffca3ba2f83e8733930407a01a3550aad9
Sam Brands <d286050@edu.rocwb.nl>
2022-11-10 16:49:42 +0100

    Updated seeders for sprint review

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             CompanySeeder::class,
+            ContractSeeder::class
         ]);
     }
 }

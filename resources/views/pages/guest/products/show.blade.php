5d79110a48017a314f830f9166bc7011febd6e26
toonvanderlely <75312183+toonvanderlely@users.noreply.github.com>
2022-11-15 10:43:42 +0100

    added the right images

--- resources/views/pages/guest/products/show.blade.php
+++ resources/views/pages/guest/products/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container px-4 px-lg-5 my-5">
     <div class="row gx-4 gx-lg-5 align-items-center">
-        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://www.recensiekoning.nl/wp-content/uploads/2019/01/Beste-koffiebonen-2019-768x524.png.webp" alt="..." /></div>
+        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{ asset($product->image_path) }}" alt="..." /></div>
         <div class="col-md-6">
             <div class="small mb-1">Barroc Intens</div>
             <h1 class="display-5 fw-bolder">{{$product->name}}</h1>

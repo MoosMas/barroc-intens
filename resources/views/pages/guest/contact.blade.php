75224c4a87560e78b8234132a337d8b1e4a6ef6a
Sam Brands <d286050@edu.rocwb.nl>
2022-11-16 13:57:45 +0100

    Updated contact create form

--- resources/views/pages/guest/contact.blade.php
+++ resources/views/pages/guest/contact.blade.php
@@ -2,30 +2,32 @@
 
 @section('content')
     <div class="pt-5 container mt-3 mb-3">
-        <form class="row g-3 w-50 mx-auto">
+        <form class="row g-3 w-50 mx-auto" action="{{route('contact.store')}}" method="POST">
+            @csrf
+            
             <h2>Neem contact met ons op voor de offerte</h2>
             <div class="col-md-6">
-                <label for="inputName" class="form-label">Naam</label>
-                <input type="text" class="form-control" id="inputName">
+                <label for="name" class="form-label">Naam</label>
+                <input type="text" class="form-control" id="name" name="name">
             </div>
             <div class="col-md-6">
-                <label for="inputCompanyName" class="form-label">Bedrijfsnaam</label>
-                <input type="text" class="form-control" id="inputCompanyName">
+                <label for="company_name" class="form-label">Bedrijfsnaam</label>
+                <input type="text" class="form-control" id="company_name" name="company_name">
             </div>
             <div class="col-md-6">
-                <label for="inputEmail" class="form-label">Email</label>
-                <input type="email" class="form-control" id="inputEmail" placeholder="example@hotmail.com">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="example@hotmail.com">
             </div>
             <div class="col-md-6">
-                <label for="inputPhonenumber" class="form-label">Telefoonnummer</label>
-                <input type="tel" class="form-control" id="inputPhonenumber" placeholder="06123456789">
+                <label for="phone" class="form-label">Telefoonnummer</label>
+                <input type="tel" class="form-control" id="phone" name="phone" placeholder="06123456789">
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Opmerkingen</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                <label for="message" class="form-label">Bericht</label>
+                <textarea class="form-control" id="message" rows="5" name="message"></textarea>
             </div>
             <div class="col-12">
-                <button type="submit" class="btn btn-primary">Stuur offerte</button>
+                <input type="submit" class="btn btn-primary" value="Stuur offerte">
             </div>
         </form>
     </div>
